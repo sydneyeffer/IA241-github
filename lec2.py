ba8351e468d0450658f58676f93fa7c01e4567bd
EC2 Default User <ec2-user@ip-172-31-28-13.ec2.internal>
2023-01-26 21:08:54 +0000

    my lec2

---
+++ lec2.py
@@ -0,0 +1,42 @@
+'''
+lec 2
+numbers, strings, and varibales
+'''
+
+
+#print(  type('hello')  )  #single line comment 
+
+#print(  type(123)  )
+
+#print(  type(123.123)  )
+
+#print(  type('567.')   )
+
+#print( "  it's our 2nd phython ")
+
+#print( 'Hello World')
+
+#print( 'Hello World'.upper()     )
+
+#print( 'Hello World'.lower()     )
+
+#print( '  hello world  '.strip())
+
+#print('hello world'.split())  #default, split by spaces
+
+#print('hello world'.split('o'))  # split by "o"
+
+#print('hello ' + 'world')
+
+#print(1*2)
+
+my_num = 123
+print(my_num + 32.5)
+
+
+
+
+
+
+
+
