60b906eec8b98a6aece5b08d21f17200380964e2
EC2 Default User <ec2-user@ip-172-31-28-13.ec2.internal>
2023-01-26 21:57:11 +0000

    lab2.py

---
+++ lab2.py
@@ -0,0 +1,40 @@
+'''
+lab2
+numbers, varibales, and strings
+'''
+
+#6.1
+my_name = 'Tom'
+print(my_name.upper())
+
+#6.2 
+my_id = 123
+print(my_id)
+
+#6.3
+# 123 = my_id a variable cannot start with a number.
+
+#6.4
+my_id = your_id = 123
+print(my_id)
+print(your_id)
+
+#6.5
+my_id_str = '123'
+print(my_id_str)
+
+#6.6
+# print(my_name+my_id) you canno do add a string and a number. 
+
+#6.7 
+print(my_name + my_id_str)
+
+#6.8
+print(my_name*3)
+
+#6.9
+print('hello, world. This is my first python string.'.split('.'))
+
+#6.10
+# message ='Tom's id is 123' you would need double quotation marks and it is not a proper string format
+
