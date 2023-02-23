c7df2fe7639310a5d1d607c634080e62f7493a3d
EC2 Default User <ec2-user@ip-172-31-28-13.ec2.internal>
2023-02-23 21:54:41 +0000

    lab6 submission.

---
+++ lab6.py
@@ -0,0 +1,82 @@
+'''
+
+lab6
+for loop, range function
+'''
+
+#3.1
+for i in range(6):
+    if i !=3:
+        print(i)
+    
+#3.2 
+
+result = 1
+for i in range(1,6):
+    result = result * i
+    
+print(result)
+
+#3.3
+
+result = 0
+for i in range(1,6):
+    result = result +i
+    
+print(result)
+
+#3.4
+
+result = 1
+for i in range(3,9):
+    result = result * i
+    
+print(result)
+
+#3.5
+
+result = 1
+for i in range(4,9):
+    result = result * i
+    
+print(result)
+
+#3.6
+
+result = 0
+
+for word in 'this i smy 6th string'.split():
+    # print(word)
+    result = result +1
+
+print(result)
+
+#3.7
+
+my_tweet = {
+            "favorite_count":1138, 
+            "lang": "en", 
+            "coordinates": (-75, 40), 
+            "entities": 
+                {"hashtags": 
+                    ["Preds", "Pens", "SingIntoSpring"]}  
+            }
+result = 0
+for hashtag in my_tweet['entities']['hashtags']:
+    result = result +1
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
